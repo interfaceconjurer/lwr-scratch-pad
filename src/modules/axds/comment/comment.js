9ef8271039eaffd445eaa74450ea09ecb3a06d51
Jordan L Wright <47576172+interfaceconjurer@users.noreply.github.com>
2022-03-19 02:22:48 -0400

    init skeleton to embeded

--- src/modules/axds/comment/comment.js
+++ src/modules/axds/comment/comment.js
@@ -1,5 +1,20 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class Comment extends LightningElement {
     @api comment;
+    @api loading;
+    name;
+    email;
+    body;
+    
+    
+    connectedCallback() {
+        if (this.comment){
+            console.log(this.comment);
+            this.name = this.comment.name;
+            this.email = this.comment.email;
+            this.body = this.comment.body;
+        }
+        
+    }
 }
