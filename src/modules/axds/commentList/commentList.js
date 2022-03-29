0d1aec825243dd0c80a3950b6adbc0a46d0269b8
Charlie Gleason <hi@charliegleason.com>
2022-03-29 17:58:35 +0100

    Removed skeleton generator for simplicity

--- src/modules/axds/commentList/commentList.js
+++ src/modules/axds/commentList/commentList.js
@@ -1,15 +1,6 @@
-import { LightningElement, api, track } from 'lwc'
+import { LightningElement, api } from 'lwc'
 
 export default class CommentList extends LightningElement {
   @api loading
   @api comments
-  @api skeletons = '5'
-
-  get skeleton() {
-    const output = Array(Number(this.skeletons))
-      .fill()
-      .map((element, index) => index)
-
-    return output
-  }
 }
