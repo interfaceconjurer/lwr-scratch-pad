6fa3629997d720c141f0c8bb27842e98f944baa4
Charlie Gleason <hi@charliegleason.com>
2022-03-29 15:34:57 +0100

    Found an out for the getters and settrs

--- src/modules/axds/comment/comment.js
+++ src/modules/axds/comment/comment.js
@@ -4,31 +4,8 @@ import { presentationHelper } from '../../../utils/presentationHelper'
 export default class Comment extends LightningElement {
   @api loading
   @api error
+  @api comment = { name: '', email: '', body: '', image: '' }
   @track presentationState = {}
-  name
-  body
-  email
-  image
-
-  @api
-  set comment(value) {
-    const comment = value
-    if (comment) {
-      this.name = comment.name
-      this.body = comment.body
-      this.email = comment.email
-      this.image = comment.image
-    }
-  }
-
-  get comment() {
-    return {
-      name: this.name,
-      body: this.body,
-      email: this.email,
-      image: this.image,
-    }
-  }
 
   renderedCallback() {
     this.presentationState = {
