f946c578585dfcd7b0900bc707bd2a023afeeddd
Charlie Gleason <hi@charliegleason.com>
2022-03-31 12:50:15 +0100

    Continued refining the data table

--- src/modules/axds/dataTable/dataTable.js
+++ src/modules/axds/dataTable/dataTable.js
@@ -3,4 +3,22 @@ import { LightningElement, api } from 'lwc'
 export default class CommentList extends LightningElement {
   @api loading
   @api comments
+
+  synced = false
+
+  get dummyData() {
+    return Array(5)
+      .fill()
+      .map((element, index) => {
+        return index
+      })
+  }
+
+  get isSynced() {
+    return this.synced
+  }
+
+  toggleSync() {
+    this.synced = !this.synced
+  }
 }
